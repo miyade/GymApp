a95e09d881913bf3fa22879ece516a9f52991b30
miyade <mariem.ayedi@esprit.tn>
2020-11-19 15:22:54 +0100

    hashed password

--- src/Controllers/RegisterController.js
+++ src/Controllers/RegisterController.js
@@ -1,16 +1,23 @@
 const User = require('../models/User');
-
+const bcrypt = require('bcrypt');
 module.exports={
     async store(req,res){
         try {
            const {firstName, lastName, password, email} = req.body;
-           const user = await User.create({
-            firstName:firstName,
-            lastName:lastName,
-            password:password,
-            email:email
-           });
-           return res.json(user);
+           const existingUser = await User.findOne({email});
+           if(!existingUser){
+               const hashedPassword = await bcrypt.hash(password, 10);
+            const user = await User.create({
+                firstName:firstName,
+                lastName:lastName,
+                password:hashedPassword,
+                email:email
+               });
+               return res.json(user);
+           }
+          return res.status(400).json({
+              message: 'User already exists! Do you want to login instead?'
+          });
         } catch (error) {
             throw Error(`Error while registering new user : ${error}`)
         }
