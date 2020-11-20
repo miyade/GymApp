3056d2d389fcd05a9e0eb2cf472cb94f905aeadb
miyade <mariem.ayedi@esprit.tn>
2020-11-20 11:59:24 +0100

    fixed typo

--- src/Controllers/RejectionController.js
+++ src/Controllers/RejectionController.js
@@ -1,14 +1,17 @@
 const Subscription = require('../models/Subscription');
 module.exports = {
-   async reject(req,res){
-    const { subscription_id } = req.params;
-    try {
-        const { subscription } = await Subscription.findById(subscription_id);
-        subscription.approved= false;
-        await subscription.save();
-        return res.json(subscription);
-    } catch (error) {
-        return res.status(400).json(error);
+    async reject(req,res){
+        const { subscription_id } = req.params;
+
+        try {
+            const subscription = await Subscription.findById(subscription_id);
+            subscription.approved= false;
+            await subscription.save();
+            return res.json(subscription);
+            
+        } catch (error) {
+            return res.status(400).json(error)
+        }
+        
     }
-   }
 }
